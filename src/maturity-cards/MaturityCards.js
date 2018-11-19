2d2037da2a452c5fe7f0ce1c0c0d5aa9db8bafe9
Robert Brem <brem_robert@hotmail.com>
2018-11-19 06:10:11 +0100

    [REFAC] MaturityCards refactoring

--- src/maturity-cards/MaturityCards.js
+++ src/maturity-cards/MaturityCards.js
@@ -26,70 +26,53 @@ export default class MaturityCards extends HTMLElement {
         this.cards.forEach(card => cards.appendChild(card));
     }
 
-    renderDetailTable() {
-        if (this.detailTable) {
-            this.appendChild(new MaturityTable(this.detailTable.minEfficiency, this.detailTable.minEfficiencyService.versions));
-        }
+    initializeTeamsView() {
+        this.title = '';
+        new TeamView()
+            .retrieveTeams()
+            .then(cards => {
+                this.cards = cards;
+                this.render();
+            });
     }
 
-    initialize() {
-        const hash = window.location.hash.substring(1);
-        const teamIdSeparator = hash.indexOf('/');
-        if (teamIdSeparator < 0) {
-            this.title = '';
-            new TeamView()
-                .retrieveTeams()
-                .then(cards => {
-                    this.cards = cards;
-                    this.render();
-                });
-            return;
-        }
-        let teamId = hash.substring(teamIdSeparator + 1, hash.length);
-        const maturityIdSeparator = teamId.indexOf('/');
-        if (maturityIdSeparator < 0) {
-            const retrieveBaseMaturities = new BaseMaturitiesView()
-                .retrieveBaseMaturities(teamId);
-            const retrieveTeam = this.client.retrieveTeam(teamId);
-            Promise.all([retrieveTeam, retrieveBaseMaturities])
-                .then(([team, cards]) => {
-                    this.cards = cards;
-                    this.title = `
+    initializeTeamMaturities(teamId) {
+        const retrieveBaseMaturities = new BaseMaturitiesView()
+            .retrieveBaseMaturities(teamId);
+        const retrieveTeam = this.client.retrieveTeam(teamId);
+        Promise.all([retrieveTeam, retrieveBaseMaturities])
+            .then(([team, cards]) => {
+                this.cards = cards;
+                this.title = `
                     <a href="#">
                         <h2>${team.name}</h2>
                     </a>`;
-                    this.render();
-                });
-            return;
-        }
-        const maturitiesPath = teamId.substring(maturityIdSeparator + 1, teamId.length);
-        let maturityId = maturitiesPath.substring('maturities/'.length, maturitiesPath.length);
-        teamId = teamId.substring(0, maturityIdSeparator);
-        const detailIdSeparator = maturityId.indexOf('/');
-        if (detailIdSeparator < 0) {
-            const retrieveDetailMaturities = new DetailMaturityView().retrieveDetailMaturities(teamId, maturityId);
-            const retrieveTeam = this.client.retrieveTeam(teamId);
-            const retrieveTeamMaturity = this.client.retrieveTeamMaturity(teamId, maturityId);
-            Promise.all([retrieveTeam, retrieveTeamMaturity, retrieveDetailMaturities])
-                .then(([team, maturity, cards]) => {
-                    this.title = `
+                this.render();
+            });
+    }
+
+    initializeDetailMaturities(teamId, teamMaturityId) {
+        const retrieveDetailMaturities = new DetailMaturityView().retrieveDetailMaturities(teamId, teamMaturityId);
+        const retrieveTeam = this.client.retrieveTeam(teamId);
+        const retrieveTeamMaturity = this.client.retrieveTeamMaturity(teamId, teamMaturityId);
+        Promise.all([retrieveTeam, retrieveTeamMaturity, retrieveDetailMaturities])
+            .then(([team, maturity, cards]) => {
+                this.title = `
                     <a href="#">
                         <h2>${team.name}</h2>
                     </a>
                     <a href="#teams/${team.id}">
                         <h3>${maturity.name}</h3>
                     </a>`;
-                    this.cards = cards;
-                    this.render();
-                });
-            return;
-        }
-        const detailsPath = maturityId.substring(detailIdSeparator + 1, maturityId.length);
-        const detailId = detailsPath.substring('details/'.length, detailsPath.length);
-        maturityId = maturityId.substring(0, detailIdSeparator);
-        const retrieveDetailMaturity = this.client.retrieveDetailMaturity(teamId, maturityId, detailId);
+                this.cards = cards;
+                this.render();
+            });
+    }
+
+    initializeAdditionalInformation(teamId, teamMaturityId, detailId) {
+        const retrieveDetailMaturity = this.client.retrieveDetailMaturity(teamId, teamMaturityId, detailId);
         const retrieveTeam = this.client.retrieveTeam(teamId);
-        const retrieveTeamMaturity = this.client.retrieveTeamMaturity(teamId, maturityId);
+        const retrieveTeamMaturity = this.client.retrieveTeamMaturity(teamId, teamMaturityId);
         Promise.all([retrieveTeam, retrieveTeamMaturity, retrieveDetailMaturity])
             .then(([team, teamMaturity, detailMaturity]) => {
                 this.title = `
@@ -105,6 +88,33 @@ export default class MaturityCards extends HTMLElement {
                 this.cards = [new MaturityTable(detailMaturity.minEfficiency, detailMaturity.minEfficiencyService.versions)];
                 this.render();
             });
+    }
+
+    initialize() {
+        const hash = window.location.hash.substring(1);
+        const teamIdSeparator = hash.indexOf('/');
+        if (teamIdSeparator < 0) {
+            this.initializeTeamsView();
+            return;
+        }
+        let teamId = hash.substring(teamIdSeparator + 1, hash.length);
+        const maturityIdSeparator = teamId.indexOf('/');
+        if (maturityIdSeparator < 0) {
+            this.initializeTeamMaturities(teamId);
+            return;
+        }
+        const maturitiesPath = teamId.substring(maturityIdSeparator + 1, teamId.length);
+        let maturityId = maturitiesPath.substring('maturities/'.length, maturitiesPath.length);
+        teamId = teamId.substring(0, maturityIdSeparator);
+        const detailIdSeparator = maturityId.indexOf('/');
+        if (detailIdSeparator < 0) {
+            this.initializeDetailMaturities(teamId, maturityId);
+            return;
+        }
+        const detailsPath = maturityId.substring(detailIdSeparator + 1, maturityId.length);
+        const detailId = detailsPath.substring('details/'.length, detailsPath.length);
+        maturityId = maturityId.substring(0, detailIdSeparator);
+        this.initializeAdditionalInformation(teamId, maturityId, detailId);
     }
 }
 
